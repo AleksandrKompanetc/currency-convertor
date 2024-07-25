ca71f8b522eadd540e711da9dee89e95b48b0e8a
AleksandrKompanetc <moneymacker7@gmail.com>
2024-07-25 13:09:14 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Block } from './Block';
 import './index.css';
 
 function App() {
+
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+    .then((res) => res.json())
+    .then((json) => {
+      setRates(json.rates);
+      console.log(json.rates);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert('Не удалось получить информацию');
+    });
+  }, []);
+
   return (
     <div className="App">
-      converter
+      <Block value={0} currency='RUB' onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency='USD' />
     </div>
   );
 }
